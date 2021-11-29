1b4047ea20450e4248b9e8ffa18d883dfa6741dd
Chase Wheeler <cwheeler0204@gmail.com>
2021-11-28 21:44:37 -0800

    Application converted from sending emails to sending texts

--- app.js
+++ app.js
@@ -1,18 +1,19 @@
 // Your web app's Firebase configuration
-var firebaseConfig = {
-  apiKey: "AIzaSyB6Izsku_qLcnr9lc0Ivj5eejB7-5FCaRE",
-  authDomain: "test-form-d3492.firebaseapp.com",
-  databaseURL: "https://test-form-d3492.firebaseio.com",
-  projectId: "test-form-d3492",
-  storageBucket: "test-form-d3492.appspot.com",
-  messagingSenderId: "222398070278",
-  appId: "1:222398070278:web:bf51f5c8a26dcfff9ecd87",
+const firebaseConfig = {
+  apiKey: "AIzaSyAE8vNE51eBQniLkUpUtruF0myNk4HittM",
+  authDomain: "contact-form-test-project.firebaseapp.com",
+  databaseURL: "https://contact-form-test-project-default-rtdb.firebaseio.com",
+  projectId: "contact-form-test-project",
+  storageBucket: "contact-form-test-project.appspot.com",
+  messagingSenderId: "477801383448",
+  appId: "1:477801383448:web:b2da7fd6e90c04b3a5256d"
 };
+
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
-// Refernece contactInfo collections
-let contactInfo = firebase.database().ref("infos");
+// Reference contactInfo collections
+let contactInfo = firebase.database().ref("customers");
 
 // Listen for a submit
 document.querySelector(".contact-form").addEventListener("submit", submitForm);
@@ -22,16 +23,17 @@ function submitForm(e) {
 
   // Get input Values
   let name = document.querySelector(".name").value;
-  let email = document.querySelector(".email").value;
+  let email = document.querySelector(".email").value + document.querySelector('select').value;
   let message = document.querySelector(".message").value;
-  console.log(name, email, message);
 
   saveContactInfo(name, email, message);
 
   document.querySelector(".contact-form").reset();
+
+  sendEmail(name, email, message);
 }
 
-// Save infos to Firebase
+// Save info to Firebase
 function saveContactInfo(name, email, message) {
   let newContactInfo = contactInfo.push();
 
@@ -40,4 +42,46 @@ function saveContactInfo(name, email, message) {
     email: email,
     message: message,
   });
+
+  retrieveInfo();
+}
+
+// Retrieve info
+function retrieveInfo() {
+  let ref = firebase.database().ref("customers");
+  ref.on("value", gotData);
+}
+
+function gotData(data) {
+  let customer = data.val();
+  let keys = Object.keys(customer);
+
+  for (let i = 0; i < keys.length; i++) {
+    let infoData = keys[i]
+    let name = customer[infoData].name
+    let email = customer[infoData].email
+    let message = customer[infoData].message
+
+    let infoResults = document.querySelector(".infoResults");
+
+    infoResults.innerHTML += `<div>
+    <p><strong>Name: <strong/>${name} <br/>
+    <a><strong>Email: <strong/>${email}</a> <br/>
+    <a><strong>Message: <strong/>${message}</a>
+    </p>
+    </div>`;
+  }
+}
+
+retrieveInfo();
+
+// Send email
+function sendEmail(name, email, message) {
+  Email.send({
+    SecureToken: "f1bb0320-541e-4ff7-9dcd-5fdfc67bdb7f",
+    To: `${email}`,
+    From: "mustachedbananaman@gmail.com",
+    Subject: `${name} sent you a message`,
+    Body: `${message}`,
+  }).then(message => alert("Message sent successfully"));
 }
